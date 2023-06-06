9075411f8b8d2bb7a53eb1f39b62051c6cdde4a1
aloniwe <nazar.pavlyukovich@gmail.com>
2023-06-06 14:58:17 +0300

    change favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,10 +6,10 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-{{--        <link rel="preconnect" href="https://fonts.bunny.net">--}}
-{{--        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />--}}
+        <link rel="icon" href="favicon.svg" type="image/svg+xml"/>
 
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://rsms.me">
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
         <!-- Scripts -->

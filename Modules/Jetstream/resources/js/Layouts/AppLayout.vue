a29c9daf2b4ddeb97ce3144d62bbd8a558592f19
darile <darile2202@gmail.com>
2023-06-30 15:16:04 +0300

    fix compilations

--- Modules/Jetstream/resources/js/Layouts/AppLayout.vue
+++ Modules/Jetstream/resources/js/Layouts/AppLayout.vue
@@ -91,12 +91,6 @@ const logout = () => {
                                 </NavLink>
                             </div>
 
-                            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                                <NavLink :href="route('api.index')" :active="route().current('teams.index')">
-                                    {{$t('Teams')}}
-                                </NavLink>
-                            </div>
-
                         </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ml-6">

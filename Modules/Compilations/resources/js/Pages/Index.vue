02d045db994b7065b7080f643b8f59e868733515
darile <darile2202@gmail.com>
2023-07-04 22:06:21 +0300

    improve copilations

--- Modules/Compilations/resources/js/Pages/Index.vue
+++ Modules/Compilations/resources/js/Pages/Index.vue
@@ -2,12 +2,32 @@
 import AppLayout from 'Jetstream/Layouts/AppLayout.vue';
 import DraggableList from '../Components/DarggableList.vue'
 import ManageItems from "../../../../OpenAI/resources/js/Components/ManageItems.vue";
-
-
+import SelectOrCreateCompolations from "../Components/SelectOrCreateCompolations.vue";
+import {ref, watch} from 'vue'
 const props = defineProps({
     presets: Array,
     previewItem: Object,
+    complications: Array
 });
+
+console.log(props.previewItem, "ID")
+
+const loader = ref(true);
+
+const dataRight = ref();
+const activeCompilations = ref();
+const itemRightData = (data) => {
+    dataRight.value = data;
+    console.log(data)
+}
+
+const selectedPresetData = async (data) => {
+    loader.value = true; // Встановлюємо loader у стан загрузки
+    activeCompilations.value = await props.complications.find(item => item.id === data);
+    loader.value = false; // Встановлюємо loader у стан завантаження завершено
+}
+
+
 </script>
 
 <template>
@@ -15,12 +35,22 @@ const props = defineProps({
         <template #header>
             <div class="flex">
                 <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                    {{ $t('compilations') }}
+                    {{ $t('Compilations') }}
                 </h2>
-                <ManageItems :presets="presets"/>
             </div>
-
         </template>
-        <DraggableList :presets="presets" :previewItem="previewItem"/>
+        <div class="max-w-7xl mx-auto my-auto py-10 sm:px-6 lg:px-8">
+            <div class="bg-white shadow sm:rounded-lg">
+                <div class="mx-auto px-6 py-6">
+                    <SelectOrCreateCompolations :complications="complications" @selectedPreset="selectedPresetData" :positions="dataRight" />
+                    <div class="text-center mt-5" v-if="loader || !activeCompilations">
+                        <div>Select or create a new compilation</div>
+                    </div>
+                    <div class="grid grid-cols-12" v-else>
+                        <DraggableList :presets="presets" :previewItem="previewItem" :compilation="activeCompilations" @itemRight="itemRightData" />
+                    </div>
+                </div>
+            </div>
+        </div>
     </AppLayout>
 </template>

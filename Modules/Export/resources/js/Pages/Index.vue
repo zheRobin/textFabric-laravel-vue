449329fb13afce16329965c8eab764b3a4420cb4
darile <darile2202@gmail.com>
2023-07-31 23:48:57 +0300

    fix from basecamp

--- Modules/Export/resources/js/Pages/Index.vue
+++ Modules/Export/resources/js/Pages/Index.vue
@@ -15,6 +15,7 @@ import {options} from "Modules/Export/resources/js/optionsForDownload";
 import ConfirmationModal from "Jetstream/Components/ConfirmationModal.vue";
 import DangerButton from "Jetstream/Components/DangerButton.vue";
 import Pagination from "Modules/Jetstream/resources/js/Components/Pagination.vue";
+import {MinusCircleIcon, EyeIcon} from "@heroicons/vue/20/solid";
 
 const props = defineProps({
     languages: Array,
@@ -254,7 +255,7 @@ const search = (event) => {
                     <div class="flex border-b border-gray-200 pb-8 items-center">
                         <label class="mr-2 mt-1 font-medium dark:text-white">{{$t('Compilation')}}:</label>
                         <SelectMenu @update:modelValue="changePreset" v-model="form.compilations" :options="dataLabel" id="employees" class="w-60" />
-                        <PrimaryButton class="ml-2 gap-x-1.5" @click="generate">
+                        <PrimaryButton v-if="!loading" class="ml-2 gap-x-1.5" @click="generate">
                             Generate
                         </PrimaryButton>
                     </div>
@@ -294,16 +295,25 @@ const search = (event) => {
                                         <p class="text-sm leading-6 text-gray-900">
                                             <PrimaryButton @click="showViewModal(item)" class="ml-2 gap-x-1.5">
                                                 View
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
+                                                    <path d="M6 3a3 3 0 00-3 3v1.5a.75.75 0 001.5 0V6A1.5 1.5 0 016 4.5h1.5a.75.75 0 000-1.5H6zM16.5 3a.75.75 0 000 1.5H18A1.5 1.5 0 0119.5 6v1.5a.75.75 0 001.5 0V6a3 3 0 00-3-3h-1.5zM12 8.25a3.75 3.75 0 100 7.5 3.75 3.75 0 000-7.5zM4.5 16.5a.75.75 0 00-1.5 0V18a3 3 0 003 3h1.5a.75.75 0 000-1.5H6A1.5 1.5 0 014.5 18v-1.5zM21 16.5a.75.75 0 00-1.5 0V18a1.5 1.5 0 01-1.5 1.5h-1.5a.75.75 0 000 1.5H18a3 3 0 003-3v-1.5z" />
+                                                </svg>
+
                                             </PrimaryButton>
                                             <PrimaryButton @click="showModal(item.id, item.value)" class="ml-2 gap-x-1.5">
                                                 Translation
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
+                                                    <path fill-rule="evenodd" d="M9 2.25a.75.75 0 01.75.75v1.506a49.38 49.38 0 015.343.371.75.75 0 11-.186 1.489c-.66-.083-1.323-.151-1.99-.206a18.67 18.67 0 01-2.969 6.323c.317.384.65.753.998 1.107a.75.75 0 11-1.07 1.052A18.902 18.902 0 019 13.687a18.823 18.823 0 01-5.656 4.482.75.75 0 11-.688-1.333 17.323 17.323 0 005.396-4.353A18.72 18.72 0 015.89 8.598a.75.75 0 011.388-.568A17.21 17.21 0 009 11.224a17.17 17.17 0 002.391-5.165 48.038 48.038 0 00-8.298.307.75.75 0 01-.186-1.489 49.159 49.159 0 015.343-.371V3A.75.75 0 019 2.25zM15.75 9a.75.75 0 01.68.433l5.25 11.25a.75.75 0 01-1.36.634l-1.198-2.567h-6.744l-1.198 2.567a.75.75 0 01-1.36-.634l5.25-11.25A.75.75 0 0115.75 9zm-2.672 8.25h5.344l-2.672-5.726-2.672 5.726z" clip-rule="evenodd" />
+                                                </svg>
                                             </PrimaryButton>
                                             <PrimaryButton @click="showDownloadModal(item.id)" class="ml-2 gap-x-1.5">
                                                 Download
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-4" id="download"><path fill="white" d="M21,14a1,1,0,0,0-1,1v4a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V15a1,1,0,0,0-2,0v4a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V15A1,1,0,0,0,21,14Zm-9.71,1.71a1,1,0,0,0,.33.21.94.94,0,0,0,.76,0,1,1,0,0,0,.33-.21l4-4a1,1,0,0,0-1.42-1.42L13,12.59V3a1,1,0,0,0-2,0v9.59l-2.29-2.3a1,1,0,1,0-1.42,1.42Z"></path></svg>
                                             </PrimaryButton>
-                                            <SecondaryButton @click="showModalDelete(item.id)" class="ml-2 gap-x-1.5">
+                                            <DangerButton @click="showModalDelete(item.id)" class="ml-2 gap-x-1.5">
                                                 Delete
-                                            </SecondaryButton>
+                                                <MinusCircleIcon class="-mr-0.5 w-4" aria-hidden="true" />
+                                            </DangerButton>
                                         </p>
                                     </div>
                                 </li>

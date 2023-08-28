a0e04e77642ac733312e5474c92da929668df792
Raif Kablaoui <atlassian@kp.technology>
2023-08-28 22:59:48 +0200

    eb enhancement

---
+++ .platform/hooks/postdeploy/02-npm-install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash -xe
+echo "Installing NPM Packages.."
+
+# Install NPM Packages
+cd /var/app/current
+# npm install
+
+echo "NPM Packages Installed.."
+
+# Run NPM Build
+# echo "Building Assets.."
+# npm run build

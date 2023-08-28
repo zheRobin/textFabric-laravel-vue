42739f29d363921b74c30e5b33bf89b3920f1720
Raif Kablaoui <atlassian@kp.technology>
2023-08-28 14:16:33 +0200

    addewd additional eb commands

--- .platform/hooks/postdeploy/log-file-permission.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-# echo "Giving Permission to Laravel Log file.."
-# sudo touch /var/app/current/storage/logs/laravel.log
-# sudo chown $USER:webapp /var/app/current/storage/logs/laravel.log

7ccfae40ef54add4ff19134133db4e1273d5ace4
Raif Kablaoui <atlassian@kp.technology>
2023-09-03 09:17:31 +0200

    eb settings

--- .platform/hooks/postdeploy/01-log-file-permission.sh
+++ .platform/hooks/postdeploy/01-log-file-permission.sh
@@ -3,4 +3,4 @@ echo "Giving Permission to Laravel Log file.."
 sudo touch /var/app/current/storage/logs/laravel.log
 # sudo chown $USER:webapp /var/app/current/storage/logs/laravel.log
 sudo chown -R $USER:webapp /var/app/current/storage
-sudo chmod -R 755 $USER:webapp /var/app/current/storage
+sudo chmod -R 755 /var/app/current/storage

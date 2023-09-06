61602d1794a4f9bfba4988b6373db6b5395eedf0
Raif Kablaoui <atlassian@kp.technology>
2023-09-06 21:46:04 +0200

    change worker.log permissions

--- .platform/hooks/postdeploy/01-file-permission.sh
+++ .platform/hooks/postdeploy/01-file-permission.sh
@@ -9,12 +9,9 @@ if [ ! -f /var/app/current/storage/logs/laravel.log ]; then
     chmod 644 /var/app/current/storage/logs/laravel.log
 fi 
 
-if [ ! -f /var/app/current/storage/logs/laravel.log ]; then
-    echo "Creating /storage/logs/laravel.log..."
-    touch /var/app/current/storage/logs/laravel.log
-    chown webapp:webapp /var/app/current/storage/logs/laravel.log
-    chmod 644 /var/app/current/storage/logs/laravel.log
+if [ ! -f /var/app/current/storage/logs/worker.log ]; then
+    echo "Creating /storage/logs/worker.log..."
+    touch /var/app/current/storage/logs/worker.log
+    chown webapp:webapp /var/app/current/storage/logs/worker.log
+    chmod 644 /var/app/current/storage/logs/worker.log
 fi 
-
-sudo chown webapp:webapp /var/app/current/storage/logs/worker.log
-sudo chmod 644 /var/app/current/storage/logs/worker.log

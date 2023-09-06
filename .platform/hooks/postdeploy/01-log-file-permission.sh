560e4ffe05022c1644a8c39e0f2632ddc1a08f06
Raif Kablaoui <atlassian@kp.technology>
2023-09-06 17:21:11 +0200

    beanstalk adjustments

--- .platform/hooks/postdeploy/01-log-file-permission.sh
+++ .platform/hooks/postdeploy/01-log-file-permission.sh
@@ -1,4 +1,10 @@
-#!/usr/bin/bash
-echo "Giving Permission to Laravel Log file.."
-sudo touch /var/app/current/storage/logs/laravel.log
-sudo chown $USER:webapp /var/app/current/storage/logs/laravel.log
+#!/usr/bin/bash -xe
+echo "Making /storage writeable..."
+chmod -R 755 /var/app/current/storage
+
+if [ ! -f /var/app/current/storage/logs/laravel.log ]; then
+    echo "Creating /storage/logs/laravel.log..."
+    touch /var/app/current/storage/logs/laravel.log
+    chown webapp:webapp /var/app/current/storage/logs/laravel.log
+    chmod 644 /var/app/current/storage/logs/laravel.log
+fi 

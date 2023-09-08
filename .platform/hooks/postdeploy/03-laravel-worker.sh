9d33b8e1d228996c79ff514828a94daf818948a6
Raif Kablaoui <atlassian@kp.technology>
2023-09-08 21:19:33 +0200

    eb adjustment for php 8.2

--- .platform/hooks/postdeploy/03-laravel-worker.sh
+++ .platform/hooks/postdeploy/03-laravel-worker.sh
@@ -1,17 +1,16 @@
-#!/usr/bin/bash -xe
-
-# shutdown supervisor if it is running
-echo "Shutting down Supervisor.."
-supervisorctl shutdown
+#!/usr/bin/bash
 
 # Start supervisor
 echo "Starting Supervisor.."
-supervisord -c /etc/supervisord.conf
+supervisord -c /etc/supervisor/supervisord.conf
 
 echo "Supervisor Started.."
 
 # update supervisor config
 echo "Updating Supervisor Config.."
+
+supervisorctl reread
+
 supervisorctl update
 
 # check supervisor status
@@ -19,4 +18,4 @@ echo "Checking Supervisor Status.."
 supervisorctl status
 
 # Start Laravel Queue Worker using Supervisor
-# echo "Starting Laravel Queue Worker.."
+echo "Starting Laravel Queue Worker.."

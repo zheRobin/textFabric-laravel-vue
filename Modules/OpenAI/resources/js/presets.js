f4a17ed9da847412e4f468ebc9f245632f54b4a0
Raif Kablaoui <atlassian@kp.technology>
2023-09-11 14:29:54 +0000

    adjust demo presets

--- Modules/OpenAI/resources/js/presets.js
+++ Modules/OpenAI/resources/js/presets.js
@@ -3,7 +3,7 @@ export const defaultPresets = {
         {
             slug: 'headline',
             name: 'Headline',
-            system_prompt: 'Write a one-line title about the following product. Do not use quotation marks.',
+            system_prompt: 'Write a one-line title about the following product. Do not use any quotation marks.',
             user_prompt: '@Product group',
         },
         {
@@ -15,7 +15,7 @@ export const defaultPresets = {
         {
             slug: 'bulletpoints',
             name: 'Bulletpoints',
-            system_prompt: 'Write 3 bullet points with one sentence each about the most important features of the product and its characteristics. Use dashes.',
+            system_prompt: 'Write 3 bullet points with one sentence each about the most important features of the product and its characteristics. Use only dashes.',
             user_prompt: '@Product group\n' +
                 '@Product\n' +
                 '@Feature 1\n' +
@@ -53,7 +53,7 @@ export const defaultPresets = {
         {
             slug: 'bulletpoints',
             name: 'Bulletpoints',
-            system_prompt: 'Schreibe 3 Bulletpoints mit jeweils einem Satz über die wichtigsten Features zu dem genanten Produkt und dessen Eigenschaften. Verwende Bindestriche.',
+            system_prompt: 'Schreibe 3 Bulletpoints mit jeweils einem Satz über die wichtigsten Features zu dem genanten Produkt und dessen Eigenschaften. Verwende nur Bindestriche.',
             user_prompt: '@Produktgruppe\n' +
                 '@Produkt\n' +
                 '@Merkmal 1\n' +

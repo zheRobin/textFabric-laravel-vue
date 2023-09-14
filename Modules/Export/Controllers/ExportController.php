1f57d5fed30d1835e58f443ecd717a6d679b7aaa
Muhammad Waheed <waheedup74@gmail.com>
2023-09-14 21:53:04 +0100

    Removed `queue:retry-batch` command

--- Modules/Export/Controllers/ExportController.php
+++ Modules/Export/Controllers/ExportController.php
@@ -102,10 +102,6 @@ class ExportController extends Controller
                 if ($batch->hasFailures()) {
                     // TODO: remove debug message
                     info(sprintf("[%s@%s] Batch %s has failed jobs", get_called_class(), 'generate', $batch->id));
-
-                    // TODO: we may not need this, this makes extra attempts after a job is failed,
-                    //  extra attempts will have affect on user's subscription usage without his knowledge
-                    // Artisan::call('queue:retry-batch', ['id' => $batch->id]);
                 }
                 if ($batch->pendingJobs === 0 && count($batch->failedJobIds) === 0) {
                     // TODO: remove debug message
@@ -162,10 +158,6 @@ class ExportController extends Controller
                 if ($batch->hasFailures()) {
                     // TODO: remove debug message
                     info(sprintf("[%s@%s] Batch %s has failed jobs", get_called_class(), 'translate', $batch->id));
-
-                    // TODO: we may not need this, this makes extra attempts after a job is failed,
-                    //  extra attempts will have affect on user's subscription usage without his knowledge
-                    // Artisan::call('queue:retry-batch', ['id' => $batch->id]);
                 }
                 if ($batch->pendingJobs === 0 && count($batch->failedJobIds) === 0) {
                     // TODO: remove debug message

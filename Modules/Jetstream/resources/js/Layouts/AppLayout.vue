df47af0f0eda4ce36dd121c35f2e86a40e6627e5
Muhammad Waheed <waheedup74@gmail.com>
2023-09-21 21:00:41 +0100

    Fixed long collection names to 12rem in navbar, truncated, shown full on hover

--- Modules/Jetstream/resources/js/Layouts/AppLayout.vue
+++ Modules/Jetstream/resources/js/Layouts/AppLayout.vue
@@ -114,10 +114,10 @@ const logout = () => {
                                 <Dropdown align="right" width="60">
                                     <template #trigger>
                                         <span class="inline-flex rounded-md">
-                                            <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                                            <button type="button" :title="currentCollection ? currentCollection.name : $t('Collections')" class="max-2-lines inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
                                                 {{ currentCollection ? currentCollection.name : $t('Collections') }}
 
-                                                <svg class="ml-2 -mr-0.5 h-4 w-4 min-w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                <svg class="ml-2 -mr-0.5 h-4 w-4 min-w-4 self-center" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
                                                 </svg>
                                             </button>
@@ -466,3 +466,17 @@ const logout = () => {
         </div>
     </div>
 </template>
+
+<style>
+.max-2-lines {
+    padding: 0;
+    width: 12rem;
+    max-height: 49px;
+    align-items: flex-start;
+    justify-content: flex-end;
+    text-overflow: ellipsis;
+    word-wrap: break-word;
+    overflow: hidden;
+    border: 8px solid white;
+}
+</style>

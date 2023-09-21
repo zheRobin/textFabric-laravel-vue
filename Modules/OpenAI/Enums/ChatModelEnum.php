9fc42745125c88062b7a373b6b53c7d02781f0cb
Raif Kablaoui <atlassian@kp.technology>
2023-09-21 10:32:12 +0200

    add gpt4 again

--- Modules/OpenAI/Enums/ChatModelEnum.php
+++ Modules/OpenAI/Enums/ChatModelEnum.php
@@ -13,10 +13,12 @@ enum ChatModelEnum: string
 *
 *    case GPT_4_32K_0314 = 'gpt-4-32k-0314';
 *
-*    case GPT_3_5_TURBO_0301 = 'gpt-3.5-turbo-0301';    
+*    case GPT_3_5_TURBO_0301 = 'gpt-3.5-turbo-0301';
 */
 
     case GPT_3_5_TURBO = 'gpt-3.5-turbo';
+
+    case GPT_4 = 'gpt-4';
 
 
 

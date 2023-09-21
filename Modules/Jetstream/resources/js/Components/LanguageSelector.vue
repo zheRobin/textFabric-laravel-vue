f80988a8938706f375eba205e03fa67c4713c17a
Muhammad Waheed <waheedup74@gmail.com>
2023-09-21 20:59:29 +0100

    Fixed dropdown menu width to 60rem in navbar

--- Modules/Jetstream/resources/js/Components/LanguageSelector.vue
+++ Modules/Jetstream/resources/js/Components/LanguageSelector.vue
@@ -9,7 +9,7 @@
                     >
                         <span class="locale-flag">{{ locale === 'en' ? '🇬🇧' : '🇩🇪' }}</span>
                         <svg
-                            class="ml-2 -mr-0.5 h-4 w-4"
+                            class="ml-2 -mr-0.5 h-4 w-4 min-w-4"
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
                             viewBox="0 0 24 24"

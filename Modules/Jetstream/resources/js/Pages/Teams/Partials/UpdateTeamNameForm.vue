2577a0c9f0401bc226ee4ef9372ba81a9090292d
Muhammad Waheed <waheedup74@gmail.com>
2023-09-23 10:53:15 +0100

    Removed team logo in Team Settings page

--- Modules/Jetstream/resources/js/Pages/Teams/Partials/UpdateTeamNameForm.vue
+++ Modules/Jetstream/resources/js/Pages/Teams/Partials/UpdateTeamNameForm.vue
@@ -40,9 +40,7 @@ const updateTeamName = () => {
                 <InputLabel value="Team Owner" />
 
                 <div class="flex items-center mt-2">
-                    <img class="w-12 h-12 rounded-full object-cover" :src="team.owner.profile_photo_url" :alt="team.owner.name">
-
-                    <div class="ml-4 leading-tight">
+                    <div class="leading-tight">
                         <div class="text-gray-900 dark:text-white">{{ team.owner.name }}</div>
                         <div class="text-gray-700 dark:text-gray-300 text-sm">
                             {{ team.owner.email }}

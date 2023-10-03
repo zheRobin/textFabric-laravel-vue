0b04cbdb79f0da77be6c6f741559684e3f4167c6
Raif Kablaoui <atlassian@kp.technology>
2023-10-03 19:40:59 +0200

    add woodpecker

---
+++ app/Jobs/SendNewTeamAccountToWoodpecker.php
@@ -0,0 +1,51 @@
+<?php
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Http;
+
+class SendNewTeamAccountToWoodpecker implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $email;
+    protected $firstname;
+    protected $lastname;
+
+    public function __construct($email, $firstname, $lastname)
+    {
+        $this->email = $email;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+    }
+
+    public function handle()
+    {
+
+        $woodpeckerAuth = explode(';', config('app')['woodpecker_auth']);
+        $woodpeckerCampaignId = explode(';', config('app')['woodpecker_compaign_id']);
+
+        $response = Http::withHeaders([
+            'Authorization' => 'Basic MjkwODY2LmUwZWJlM2Y4OGQ1YWEwNmQyMjQ5NzQwYWM3MDc2NjMxNzY1ZGEzNjAzOGViYjBiZmNiYzhjMzljZjkyYjkwNjQ'
+        ])->post('https://api.woodpecker.co/rest/v1/add_prospects_campaign', [
+            "campaign" => [
+            "campaign_id" => 1888480
+            ],
+            "update" => "true",
+            "prospects" => [
+            [
+               "email" => $this->email,
+               "first_name" => $this->firstname,
+               "last_name" => $this->lastname,
+               "status" => "ACTIVE"
+            ]
+         ]
+        ]);
+
+    }
+}

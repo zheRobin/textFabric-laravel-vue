ffd5b04833642428320be69c5f71abb9618f92e1
Raif Kablaoui <atlassian@kp.technology>
2023-10-05 17:13:58 +0000

    adjust api rate limit

--- Modules/RestApi/Config/rest-api.php
+++ Modules/RestApi/Config/rest-api.php
@@ -2,5 +2,5 @@
 
 return [
     'API_KEY'           => env('OPENAI_API_KEY'),
-    'API_REQUEST_LIMIT' => env('REST_API_REQUEST_LIMIT', 30000),
+    'API_REQUEST_LIMIT' => env('REST_API_REQUEST_LIMIT', 60),
 ];

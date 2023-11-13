79d27650b3a1678991da8a372467d2c9e14d295b
Raif Kablaoui <atlassian@kp.technology>
2023-11-13 15:55:36 +0100

    change supervisor config

---
+++ .platform/hooks/postdeploy/03-laravel-worker.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+# Start supervisor
+echo "Starting Supervisor.."
+sudo supervisord -c /etc/supervisor/supervisord.conf
+
+echo "Supervisor Started.."
+
+# update supervisor config
+echo "Updating Supervisor Config.."
+
+supervisorctl reread
+
+supervisorctl update
+
+supervisorctl restart all
+
+# check supervisor status
+echo "Checking Supervisor Status.."
+supervisorctl status
+
+# Start Laravel Queue Worker using Supervisor
+echo "Starting Laravel Queue Worker.."
